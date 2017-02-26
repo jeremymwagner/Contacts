d267080f77f373f51f6b604936a902cca0ca1abb
Jeremy Wagner <Jeremy@localhost.localdomain>
2017-02-26 15:50:43 -0600

    Got removal working and added filters that execute when clicking column titles

--- app.js
+++ app.js
@@ -17,9 +17,41 @@ var contactList =  [
                             email: 'sallygrn@lala.land',
                             isSelected: false,
                             id: 2
+                        },
+                        {
+                            firstName: 'Al',
+                            lastName: 'Spotts',
+                            phoneNum: 7482671398,
+                            email: 'alspot@kewl.com',
+                            isSelected: false,
+                            id: 3
+                        },
+                        {
+                            firstName: 'Bruce',
+                            lastName: 'Sandal',
+                            phoneNum: 2498789123,
+                            email: 'bsand@beach.ball',
+                            isSelected: false,
+                            id: 4
+                        },
+                        {
+                            firstName: 'Jennifer',
+                            lastName: 'Carson',
+                            phoneNum: 4429067894,
+                            email: 'jcar@nas.car',
+                            isSelected: false,
+                            id: 5
+                        },
+                        {
+                            firstName: 'Stephanie',
+                            lastName: 'Bagel',
+                            phoneNum: 9874482398,
+                            email: 'sbahel@cream.cheese',
+                            isSelected: false,
+                            id: 6
                         }
                 ];
-var idcount = 3;
+var idcount = 7;
 
 //
 // Routing for all of the pages. The view being injected to is the entire page
@@ -45,7 +77,7 @@ contactsApp.config(function($routeProvider){
 });
 
 
-contactsApp.controller('mainController', function($scope,filterFilter,$mdDialog,$location){
+contactsApp.controller('mainController', function($scope,$filter,$mdDialog,$location){
     $scope.contactList = contactList;
     $scope.tempFirst;
     $scope.tempLast;
@@ -104,10 +136,27 @@ contactsApp.controller('mainController', function($scope,filterFilter,$mdDialog,
         return list.indexOf(item) > -1;
       };
 
+//
+//  Function to remove all contacts that have been checked
+//
       $scope.removeContacts = function() {
-        contactList = contactList.filter(function(contact){
-            return !contact.id == '1';
-        });
+         var selectStop = $scope.selectedContacts.length
+         var contactStop = $scope.contactList.length
+
+         while($scope.selectedContacts.length != 0){
+             var i = $scope.selectedContacts[0]
+             for(var j = 0;j < $scope.contactList.length;j++){
+                 if(i.id == $scope.contactList[j].id){
+                     $scope.contactList.splice(j,1)
+                     $scope.selectedContacts.splice(0,1)
+                 }
+             }
+         }
+
+      }
+
+      $scope.filterBy = function(x){
+          $scope.myFilterBy = x
       }
 
     $scope.count = idcount;
